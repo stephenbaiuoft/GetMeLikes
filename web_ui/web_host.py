13de6c30886055a9332b5835a4cc421c8484bdce
stephen bai <stephenbai91@gmail.com>
2018-02-05 01:59:47 -0800

    finished report UI without javascript swithing

--- web_ui/web_host.py
+++ web_ui/web_host.py
@@ -19,6 +19,8 @@ def get_db():
 # Display an HTML page with links
 @webapp.route('/main', methods=['GET','POST'])
 def main():
+    # init database already
+    get_db()
     print("entering main function")
     return render_template("main.html", title="Landing Page")
 
@@ -26,64 +28,104 @@ def main():
 
 @webapp.route('/bartest')
 def show_bar_test():
-    word_labels = [ "Hey realDonaldTrump \nthis is second point" ,
-                    'realDonaldTrump Merylsayshi',
-                    'Tonight',
+    # now make sure it's a list of lists!!!!!
+    word_labels = [ ["My", "long", "long", "long", "label"] ,
+                    ['realDonaldTrump Merylsayshi'],
+                    ['Tonight'],
                     ]
     count_values = [250864,
                    189237,
-                   119746
-                   ]
+                   119746]
     print(count_values)
     print(word_labels)
     # do donald trump for now
     return render_template('bar.html', user_name='Testing',
                            type_name="retweets",
-                           values=count_values, labels=word_labels)
+                           values=[250864,189237,119746], labels=word_labels)
 
 
 # show top_word_list, given an id
 @webapp.route('/top_word_report', methods=['GET','POST'])
 # Display random key word count for some id
 def top_word_list():
+    # add _word as an array form to _list
+    # if the _word is too long
+    def add_to_display_list(_list, _word):
+        if len(_word) <=20:
+            _list.append([_word])
+            return
+
+        l = len(_word)
+        _s = 0
+        _e = 1
+        element = []
+        while _s < l/20:
+            if _s == 0:
+                element.append(_word[_s*20: _e*20])
+            else:
+                element.append('-'+_word[_s * 20: _e * 20])
+            _s = _e
+            _e += 1
+        _list.append(element)
+
     user_name = request.form['user_name']
+    date = request.form['date']
     count_values = []
     word_labels = []
     # do donald trump for now
     db_session = get_db()
 
-    top_limit = 10
+    top_limit = 5
     sql_cmd = "select rt_entity_list from demo_top_list where user_name =" \
               " \'" + user_name + "\' ALLOW FILTERING;"
-    try:
 
+    sql_cmd_bytime = "select creation_date, rt_entity_list from demo_top_quarter_list"\
+                     +" where user_name =" \
+              " \'" + user_name + "\' and creation_date < \'" + date + "\' ;"
+
+    print(sql_cmd_bytime)
+
+    try:
         rows = db_session.execute(sql_cmd)
         for row in rows:
             for _tuple in row[0]:
-                #print("tuple_value is: ", _tuple)
-                if top_limit < 0:
+                print("tuple_value is: ", _tuple)
+                if top_limit == 0:
                     break
                 top_limit -= 1
-                word_labels.append(_tuple[0])
                 count_values.append(_tuple[1])
+                add_to_display_list(word_labels, _tuple[0])
 
         # debugging purpose
         print(word_labels)
         print(count_values)
-        # word_labels = ['Hey realDonaldTrump', 'realDonaldTrump Merylsayshi', 'Tonight']
-        # count_values = [250864, 189237, 119746]
+
+        # time series query
+        rows = db_session.execute(sql_cmd_bytime)
+        # only 1
+        x_ary = []
+        top_word_date_ary = []
+
+        for row in rows:
+            x_ary.append([str(row[0]), row[1][0][0]])
+            top_word_date_ary.append(row[1][0][1])
+
         return render_template('bar_display.html', user_name='Jimmy Kimmel',
                                type_name="retweets",
                                values=count_values,
-                               labels=word_labels)
+                               labels=word_labels,
+                               x_ary=x_ary,
+                               top_word_date_ary=top_word_date_ary
+                               )
 
     except Exception as e:
         print("An Error Occur: default values")
         # or pop a window?
 
-        word_labels = ['Hey realDonaldTrump', 'realDonaldTrump Merylsayshi', 'Tonight']
+        word_labels = [['Hey realDonaldTrump'], ['realDonaldTrump Merylsayshi'],
+                       ['Tonight']]
         count_values = [250864, 189237, 119746]
-        return render_template('bar_test.html', user_name='Query Went Wrong',
+        return render_template('bar_display.html', user_name='Query Went Wrong',
                                type_name="retweets",
                                values=count_values, labels=word_labels)
 
